98e5c08251781867e33b61e34f783f18ffbda834
mnhhann <maih73283@gmail.com>
2025-08-29 13:07:26 +0700

    country

--- src/services/types/country.service.ts
+++ src/services/types/country.service.ts
@@ -5,11 +5,13 @@ import type { AxiosResponse } from "axios"
 export class CountryService {
   private readonly baseUrl = '/au-country'
 
-  async getAll(): Promise<Country[]> {
-    const response: AxiosResponse<{ data: Country[] }> = await apiService.get(this.baseUrl)
-    console.log('Response: ', response)
-    return response.data.data 
-  }
+    async getAbbreviations(params?: undefined): Promise<Country[]> {
+      const response = await apiService.get<Country[]>(this.baseUrl, {
+        params,
+      })
+  
+      return response
+    }
 }
 
 export const countriesService = new CountryService()
